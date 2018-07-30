cbd2a60a23e9f0e08600bfae6569ace0a544d8ad
Catherine Clague <845870941@bun2-206-17.local>
2018-07-31 08:50:06 +1200

    Pie chart, gender

--- json/js/script.js
+++ json/js/script.js
@@ -1,37 +1,57 @@
-// getting JSON using pure JS
-var xhttp = new XMLHttpRequest();
+google.charts.load('current', { packages: ['corechart'] });
+google.charts.setOnLoadCallback(getJSON);
 
-xhttp.onreadystatechange = function() {
-	if (this.status == 403) {
-		console.log('Forbidden, cannot access information');
-		return;
-	} else if (this.status == 404) {
-		console.log('Error, file not found');
-		return;
-	}
+function getJSON() {
+	// getting JSON using pure JS
+	var xhttp = new XMLHttpRequest();
 
-	if (this.status == 200 && this.readyState == 4) {
-		var data = JSON.parse(this.responseText);
-		dataList(data);
-	}
-};
+	xhttp.onreadystatechange = function() {
+		if (this.status == 403) {
+			console.log('Forbidden, cannot access information');
+			return;
+		} else if (this.status == 404) {
+			console.log('Error, file not found');
+			return;
+		}
 
-xhttp.open('GET', 'data/MOCK_DATA.json', true);
-xhttp.send();
+		if (this.status == 200 && this.readyState == 4) {
+			var data = JSON.parse(this.responseText);
+			dataList(data);
+		}
+	};
+
+	xhttp.open('GET', 'data/MOCK_DATA.json', true);
+	xhttp.send();
+}
 
 function dataList(mockData) {
 	var list = document.getElementById('data');
+	var female = [];
+	var male = [];
 
 	for (var i = 0; i < mockData.length; i++) {
-		var country = mockData[i].country;
-		var count = [];
-		for (var j = 0; j < mockData.length; j++) {
-			if (country == mockData[j].country) {
-				count.push(1);
-			}
+		if (mockData[i].gender == 'Female') {
+			female.push(1);
+		} else if (mockData[i].gender == 'Male') {
+			male.push(1);
 		}
-
-		console.log(country);
-		console.log(count.length);
 	}
+
+	pieChart(female, male);
+}
+
+function pieChart(data1, data2) {
+	var data = google.visualization.arrayToDataTable([
+		['Gender', 'Totel'],
+		['Female', data1.length],
+		['Male', data2.length]
+	]);
+	var options = {
+		title: 'Gender'
+	};
+	var chart = new google.visualization.PieChart(
+		document.getElementById('pieDiv')
+	);
+
+	chart.draw(data, options);
 }
