6fe6537e5959c50225be1a3113f03040415735d5
Catherine Clague <845870941@desktop-cnc8ria.acg.ac.nz>
2018-08-01 10:57:31 +1200

    Geonet Volcanos, coordinates and size

---
+++ geonet/js/script.js
@@ -0,0 +1,55 @@
+google.charts.load('current', { packages: ['corechart'] });
+google.charts.setOnLoadCallback(getData);
+
+function getData() {
+	$.ajax({
+		url: 'https://api.geonet.org.nz/volcano/val',
+		dataType: 'json',
+		type: 'GET',
+		success: function(data) {
+			drawChart(data);
+		},
+		error: function(error) {
+			console.log('Error');
+			console.log(error);
+		}
+	});
+}
+
+function drawChart(dataJSON) {
+	// console.log(data.with['0'].content);
+	const data = new google.visualization.DataTable();
+
+	data.addColumn('string', 'volcanoID');
+	data.addColumn('number', 'Coordinates');
+	data.addColumn('number', 'Coordinates');
+	data.addColumn('string', 'Volcano Title');
+	data.addColumn('number', 'Temperature');
+
+	for (var i = 0; i < dataJSON.features.length; i++) {
+		data.addRow([
+			dataJSON.features[i].properties.volcanoID,
+			dataJSON.features[i].geometry.coordinates[0],
+			dataJSON.features[i].geometry.coordinates[1],
+			dataJSON.features[i].properties.volcanoTitle,
+			dataJSON.features[i].properties.level
+		]);
+	}
+
+	var options = {
+		title: 'Volcanos Size & Location',
+		hAxis: {
+			title: 'Longitude'
+		},
+		vAxis: {
+			title: 'Latitude'
+		},
+		legend: { position: 'right' }
+	};
+
+	var chart = new google.visualization.BubbleChart(
+		document.getElementById('bubbleChart')
+	);
+
+	chart.draw(data, options);
+}
